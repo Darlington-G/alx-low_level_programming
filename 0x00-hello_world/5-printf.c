72c03ffe1a390c1377e3a5d002712fd53be21718
Darlington Gwanzura <darlingtongwanzura@gmail.com>
2023-02-16 16:33:14 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
- #include <stdio.h>
+#include <stdio.h>
 /**
- *  main - Your program should compile without warning when using the -Wall gcc option
- *
- *  Return: Always 0 (Success)
+ * main - 5. Hello, printf
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}	
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
