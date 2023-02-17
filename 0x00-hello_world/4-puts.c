b467b3e46ad0553fbfed92c348c75278b3b7d152
Darlington Gwanzura <darlingtongwanzura@gmail.com>
2023-02-16 16:16:40 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - use the function puts your program should end with the value 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
